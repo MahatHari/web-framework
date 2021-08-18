4cc1db4a08ebdf86d393ce4ecd9a36c76e5af50d
Hari Mahat <hmahat@gmail.com>
2021-08-18 07:38:06 +0300

    before making Sync generic ... consider User Class

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,5 @@ import { User } from './models/User';
 
 const user = new User({ name: 'Besties', age: 679 });
 
-user.save();
+user.events.on('change', () => console.log('Change !!!'));
+user.events.trigger('change');
